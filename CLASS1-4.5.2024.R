88ecd880a680b17b97a663ed4f855717171d09b6
srutha keerthi <sruthakeerthi98@gmail.com>
2024-04-07 00:24:43 -0400

    first commit

---
+++ CLASS1-4.5.2024.R
@@ -0,0 +1,81 @@
+#did the first program
+
+#assigning the basic variables
+A <-5
+B <-10
+C <- A + B
+C
+
+#basic functions
+v1 <- 4.5
+v2 <- 2
+v3 <- v1 / v2
+v3
+v4 <- sqrt(v1)
+v4
+
+#new data type:string
+greet <-"Hello,"
+name <-" Bob"
+message<-paste(greet, name)
+message
+
+#basic data types
+#Integers
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 2+3i
+typeof(z)
+#character
+a <- "h"
+typeof(a)
+
+#logical
+q <- T
+typeof(q)
+p <- FALSE
+typeof(p)
+
+#Basic math functions
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+a
+b
+c
+d
+e
+f
+
+#basic logical functions
+4 <5
+10>100
+4==5
+
+result <- 4 < 5
+result
+typeof(result)
+
+#checking if its true or false 
+result <- 4 < 5
+result
+result
+
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
